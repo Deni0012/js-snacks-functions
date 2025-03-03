d776c32177f543dc153a2c129d4e411e028a9411
Deni0012 <deni.dg00@icloud.com>
2025-03-03 21:14:29 +0100

    feat: svolgimento esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -36,9 +36,36 @@ const countVowelsHarrow = word => {
     return [numberOfVowels, vowels];
 }
 
+function countVowelsIncludes(word) {
+    let numberOfVowels = 0;
+    let vowels = [];
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i].toLowerCase();
+        const isVowel = "aeiou".includes(letter);
+        if (isVowel) {
+            numberOfVowels++;
+            vowels.push(letter);
+        }
+    }
+    return [numberOfVowels, vowels];
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const vowelsOfWord = countVowels(word);
+console.log(vowelsOfWord)
+
+
+const vowelsOfWordHarrow = countVowelsHarrow(word);
+console.log(`Numero di vocali: ${vowelsOfWordHarrow[0]}`)
+console.log(`Vocali: ${vowelsOfWordHarrow[1]}`)
+
+
+const vowelsOfWordIncludes = countVowelsIncludes(word);
+console.log(`Numero di vocali: ${vowelsOfWordIncludes[0]}`)
+console.log(`Vocali: ${vowelsOfWordIncludes[1]}`)
 
 
 
